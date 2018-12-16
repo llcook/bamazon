ce102b58020e40844aeefb9c5644f81902e6315d
watercolorhorse <29869241+llcook@users.noreply.github.com>
2018-12-16 12:33:01 -0700

    Created package.json. Coded sql connection in bamazonCustomer.js. bamazon() connects to database and displays items when initiated

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,14 +1,42 @@
-// 5. Then create a Node application called `bamazonCustomer.js`. Running this application will first display all of the items available for sale. Include the ids, names, and prices of products for sale.
+var mysql = require("mysql"); // NPM I COMPLETE!
+var inquirer = require("inquirer"); // NPM I COMPLETE!
 
-// 6. The app should then prompt users with two messages.
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 8889,
+    user: "root",
+    password: "root",
+    database: "bamazon"
+})
 
-//    * The first should ask them the ID of the product they would like to buy.
-//    * The second message should ask how many units of the product they would like to buy.
+connection.connect(function (err) {
+    if (err) throw err;
+    bamazon();
+});
 
-// 7. Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
+function bamazon() {
+    // displayItems() -- show all of the items available for sale: item_id, product_name, price
+    connection.query("SELECT * FROM products ORDER BY products.product_name", function (err, res) {
+        if (err) throw err;
+        console.log("Connected as ID: " + connection.threadId); // WORKS
 
-//    * If not, the app should log a phrase like `Insufficient quantity!`, and then prevent the order from going through.
+        // loop through products and log ID, product, price -- WORKS
+        for (var i = 0; i < res.length; i++) {
+            var item_id = res[i].item_id;
+            var product_name = res[i].product_name;
+            var price = res[i].price;
 
-// 8. However, if your store _does_ have enough of the product, you should fulfill the customer's order.
-//    * This means updating the SQL database to reflect the remaining quantity.
-//    * Once the update goes through, show the customer the total cost of their purchase.
+            console.log(`Product ID: ${item_id} | ${product_name} | $${price}`);
+        }
+
+        // inquirer: prompt user with two messages:
+        // ask user item_id of product_name they wanna buy
+        // how many units you wanna buy?
+
+        // check stock_quantity:
+        // if items_wanted > stock_quantity, log "Insufficient quanity" and return to the units question
+        // if items_wanted < stock_quantity, fulfill order:
+        // update bamazon.sql to reflect remaining quantity
+        // then show customer total cost of purchase
+    })
+}
